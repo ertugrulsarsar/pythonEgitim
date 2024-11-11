7ea2fa2cfc8d45e95f3e949341a384462a1eaea5
ertugrulsarsar <ertugrulsarsar@gmail.com>
2024-11-12 00:14:05 +0300

    delete

--- Python Programming/string_manipulation.py
+++
@@ -1,8 +0,0 @@
-# Kullanıcıdan bir cümle alınır, cümlenin her harfi büyük harfe çevrilerek tek tek ekrana basılır.
-
-def string_manipulasyon():
-    cumle = input("Bir cümle giriniz: ")
-    for harf in cumle:
-        print(harf.upper())
-
-string_manipulasyon()
